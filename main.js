1e78c54580609afd3414cd42a5724dba41a7949e
Napolik <rv.setra@gmail.com>
2022-07-11 15:42:37 +0300

    Add usercard

--- main.js
+++ main.js
@@ -8,7 +8,6 @@ class PostList extends HTMLElement {
     this.addEventListener('click', (e) => {
       if (e.target.tagName === 'A') {
         const id = e.target.href.split('#')[1].split('=')[1];
-        //console.log(id);
         this.getSinglePost(id);
       } else {
         this.querySelector('.posts__single-post').classList.remove('active');
@@ -60,4 +59,89 @@ class PostList extends HTMLElement {
   }
 }
 
+class UserList extends HTMLElement {
+  constructor() {
+    super()
+
+    this.getUsers();
+    this.addEventListener('click', (e) => {
+      if (e.target.tagName === 'A') {
+        const id = e.target.href.split('#')[1].split('=')[1];
+        this.getUserCard(id);
+        this.getUserPhotos(id);
+      }
+
+    });
+  }
+
+  getUsers() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(data => this.insertUsers(data) )
+  }
+
+  getUserCard(id) {
+    fetch('https://jsonplaceholder.typicode.com/users/' + id)
+        .then(response => response.json())
+        .then(data => this.showUserCard(data) )
+  }
+
+  getUserPhotos(id) {
+    fetch('https://jsonplaceholder.typicode.com/albums/' + id + '/photos')
+        .then(response => response.json())
+        .then(data => this.showUserPhotos(data) )
+  }
+
+  showUserPhotos(data) {
+    const userThumbs = this.querySelector('.user-thumbs');
+    userThumbs.innerHTML = '';
+
+    for (let i = 0; i < data.length; i++ ) {
+      const title = data[i].title;
+      const thumb = data[i].thumbnailUrl;
+      const thumbEl = document.createElement('img');
+
+      thumbEl.setAttribute('src', thumb);
+      thumbEl.setAttribute('alt', title);
+      userThumbs.append(thumbEl);
+    }
+  }
+
+  showUserCard(data) {
+    const userCard = this.querySelector('.user-card');
+    userCard.innerHTML = '';
+
+    const nameEl = document.createElement('li');
+    const usernameEl =  document.createElement('li');
+    const emailEl =  document.createElement('li');
+    const phoneEl =  document.createElement('li');
+    const websiteEl =  document.createElement('li');
+    const companyEl =  document.createElement('li');
+
+    nameEl.innerHTML = 'Name: ' + data.name;
+    usernameEl.innerHTML = 'Username: ' + data.username;
+    emailEl.innerHTML = 'Email: ' + data.email;
+    phoneEl.innerHTML = 'Phone: ' + data.phone;
+    websiteEl.innerHTML = 'Website: ' + data.website;
+    companyEl.innerHTML = 'Company: ' + data.company.name;
+
+    userCard.append(nameEl, usernameEl, emailEl, phoneEl, websiteEl, companyEl);
+
+  }
+
+  insertUsers(data) {
+    const usersCount = data.length;
+
+    for (let i = 0; i < usersCount; i++ ) {
+      const id = data[i].id;
+      const name = data[i].name;
+      const nameElement = document.createElement('li');
+
+      nameElement.innerHTML = '<a href="#id=' + id + '">' + name + '</a>';
+      this.querySelector('.user-list').append(nameElement);
+    }
+  }
+}
+
 customElements.define('post-list', PostList);
+customElements.define('user-list', UserList);
